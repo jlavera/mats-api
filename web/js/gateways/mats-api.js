3cae807c2a5a83f1e59f94922b9d89349ebcc7c9
Joaquin Lavera <jlavera@medallia.com>
2018-08-08 00:42:10 -0300

    Remove api mock

--- web/js/gateways/mats-api.js
+++ web/js/gateways/mats-api.js
@@ -1,3 +1,39 @@
-const mocked = false;
+import bluebird       from 'bluebird';
+import { superagent } from '../utils';
 
-module.exports = require(`./${mocked ? 'mats-api-mocked' : 'mats-api-live'}`);
+// const apiAddress = `${process.env.APIADDRESS ? process.env.APIADDRESS : 'http://localhost:8080'}/api/v1`;
+const apiAddress = `/api/v1`;
+
+module.exports = {
+  getCareers: () => {
+    return superagent.get(`${apiAddress}/careers`)
+      .set('Content-Type', 'application/json')
+      .then(response => response)
+      .get('body')
+    ;
+  },
+
+  getCoursesByCareer: (careerCode) => {
+    return superagent.get(`${apiAddress}/careers/${careerCode}/courses`)
+      .set('Content-Type', 'application/json')
+      .then(response => response)
+      .get('body')
+    ;
+  },
+
+  getTreeByCareer: (careerCode) => {
+    return superagent.get(`${apiAddress}/careers/${careerCode}/tree`)
+      .set('Content-Type', 'application/json')
+      .then(response => response)
+      .get('body')
+    ;
+  },
+
+  getReverseTreeByCareer: (careerCode) => {
+    return superagent.get(`${apiAddress}/careers/${careerCode}/reverseTree`)
+      .set('Content-Type', 'application/json')
+      .then(response => response)
+      .get('body')
+    ;
+  }
+};
