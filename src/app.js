2412ec2ad229a7681e6391928047d1b112c28c54
Joaquin Lavera <jlavera@medallia.com>
2018-08-18 14:48:53 -0300

    Split

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
 import * as path  from 'path';
 import express    from 'express';
 import bodyParser from 'body-parser';
-import favicon    from 'serve-favicon';
-
-import * as webpackDevMiddleware from 'webpack-dev-middleware';
-import * as webpackHotMiddleware from 'webpack-hot-middleware';
-
-// import * as webpack from 'webpack';
-// import * as config from '../webpack.config';
-
-// const compiler = webpack.default(config);
 
 module.exports = function $app(
   apiV1Router,
@@ -21,27 +12,12 @@ module.exports = function $app(
   const app = express();
   const distPath = process.env.NODE_ENV === 'aws' ? '/..' : '/../dist';
 
-  // --- FRONTEND
-
-  // app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-  // app.use(webpackHotMiddleware(compiler));
-
-  app.use(express.static(path.resolve(__dirname + distPath)));
-
-  // app.use(express.static(path.resolve('/../public')));
-
-  app.get('/', function(request, response) {
-    response.sendFile(path.resolve(__dirname + `${distPath}/index.html`))
-  });
-
   // --- API
 
   app.use(winston2LoggerMiddleware());
   app.use(bodyParser.json());
 
   // ---
-
-  // app.use(favicon(`${__dirname}/../public/favicon.ico`));
 
   app.use(apiV1Router);
 
--- web/js/utils/index.js
+++
@@ -1,7 +0,0 @@
-import superagent        from 'superagent';
-import bluebird          from 'bluebird';
-import superagentPromise from 'superagent-promise';
-
-module.exports = {
-  superagent: superagentPromise(superagent, bluebird)
-};
