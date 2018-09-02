b2f89ec59602dec2b08e037fb3457a22b146eb5d
jhl <jlavera92@gmail.com>
2018-09-02 16:40:41 -0300

    remove syslog winston

--- src/logger.js
+++ src/logger.js
@@ -9,10 +9,10 @@ module.exports = function loggerDependency(config) {
       type:  'console',
       adder: addConsoleTransport
     },
-    {
-      type:  'syslog',
-      adder: addSysLogTransport
-    }
+    // {
+    //   type:  'syslog',
+    //   adder: addSysLogTransport
+    // }
   ];
 
   transports.forEach((transport) => {
@@ -30,25 +30,25 @@ function addConsoleTransport(config, logger) {
   logger.add(winston.transports.Console, config.logger.console);
 }
 
-function addSysLogTransport(config, logger) {
-  require('winston-syslog').Syslog; // eslint-disable-line
+// function addSysLogTransport(config, logger) {
+//   require('winston-syslog').Syslog; // eslint-disable-line
 
-  // We monkey-patch syslog transport to properly
-  // map winston's `warn` level to syslog's `warning` level
-  // which is really silly and should be done by transport
-  // itself, but for now we're stuck with this.
+//   // We monkey-patch syslog transport to properly
+//   // map winston's `warn` level to syslog's `warning` level
+//   // which is really silly and should be done by transport
+//   // itself, but for now we're stuck with this.
 
-  const transport    = new winston.transports.Syslog(config.logger.syslog);
-  const transportLog = transport.log;
+//   const transport    = new winston.transports.Syslog(config.logger.syslog);
+//   const transportLog = transport.log;
 
-  transport.log = function log() {
-    if (arguments[0] === 'warn') { // eslint-disable-line
-      arguments[0] = 'warning'; // eslint-disable-line
-    }
+//   transport.log = function log() {
+//     if (arguments[0] === 'warn') { // eslint-disable-line
+//       arguments[0] = 'warning'; // eslint-disable-line
+//     }
 
-    // eslint-disable-next-line
-    return transportLog.apply(transport, arguments);
-  };
+//     // eslint-disable-next-line
+//     return transportLog.apply(transport, arguments);
+//   };
 
-  logger.add(transport, null, true);
-}
+//   logger.add(transport, null, true);
+// }
